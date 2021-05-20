07f7ed8c3d7ff0ed16824604678bf880e6b85fc6
piLeoni <leoni@mit.edu>
2021-05-20 19:04:54 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Post from "./components/post.svelte";
+
+    let contents = fetch('contents.json').then(res => res.json());
+    // import json from './public/contents.json';
+
+    // console.log(paragraphs)
+</script>
+
+<h1>app</h1>
+<!-- 
+{#each posts as post, i}
+    <Post title={post} on:delete={() => deletePost(i)} />
+{/each} -->
+
+{#await contents}
+    <p>Loading Contents</p>
+{:then result}
+
+{#each result.paragraphs as post, i}
+        <Post title={post.title} />
+    {/each}
+{/await}
---
+++ src/components/post.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    export let title = "Title";
+    export let content = `Eu reprehenderit id sit do sunt esse incididunt laborum. 
+    In reprehenderit ad adipisicing ea quis et occaecat nisi aute sunt irure. 
+    Occaecat officia duis sit dolor magna sit id. 
+    Fugiat ad magna anim esse consequat laboris aute minim laborum dolore sunt. 
+    Veniam reprehenderit consequat ipsum ad mollit nisi id enim. Enim duis ex 
+    aliquip ut exercitation nisi laborum id. Id exercitation dolor minim labore pariatur.`;
+    const dispatch = createEventDispatcher();
+
+    function deleteMe() {
+        dispatch("delete");
+    }
+</script>
+
+<div class="post">
+    <button on:click={ deleteMe}> delete</button>
+    <h3>{title.toUpperCase()}</h3>
+    <p>{content}</p>
+</div>
+
+<style>
+    .post {
+        border: 2px solid black;
+        margin: 3px;
+        padding: 5px;
+        width: 500px;
+    }
+</style>
